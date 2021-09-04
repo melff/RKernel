0206ec2061fbbee1cdea2867729077f2663d266a
Martin Elff <elff@newdebian.local>
2021-09-05 01:59:31 +0200

    Allow to specify args for datatable

--- pkg/R/datatable.R
+++ pkg/R/datatable.R
@@ -1,31 +1,45 @@
-html_table <- function(x,id=UUIDgenerate()){
+html_table <- function(x,id=UUIDgenerate(),use.rownames=TRUE,html_class="display"){
     x <- as.data.frame(x)
     rn <- row.names(x)
     nms <- names(x)
     x <- format(x)
     x <- as.matrix(x)
-    x <- cbind(rn,x)
-    nms <- c("",nms)
+    if(use.rownames){
+        x <- cbind(rn,x)
+        nms <- c("",nms)
+    }
     tbody <- structure(paste("<td>",x,"</td>"),dim=dim(x))
     tbody <- apply(tbody,1,function(row) paste0(c("<tr>",row,"</tr>"),collapse=""))
     tbody <- c("<tbody>",tbody,"</tbody>")
     thead <- paste("<th>",nms,"</th>")
     thead <- paste0(c("<tr>",thead,"</tr>"),collapse="")
     thead <- c("<thead>",thead,"</thead>")
-    table <- c(paste0("<table id='",id,"' class='display'>"),thead,tbody,"</table>")
+    table <- c(paste0("<table id='",id,"' class='",html_class,"'>"),thead,tbody,"</table>")
     table <- paste0(table,collapse="\n")
     #raw_html(table,id=id)   
     table
 }
 
-data_table <- function(x,id=UUIDgenerate()){
-    table <- html_table(x,id)
+data_table <- function(x,id=UUIDgenerate(),
+                       html_class="display",
+                       use.rownames = TRUE,
+                       searching = FALSE,
+                       scrollY = 250,
+                       paging = FALSE,
+                       scrollX = TRUE,
+                       scrollCollapse = TRUE,
+                       fixedColumns = 1,
+                       ...
+                       ){
+    table <- html_table(x,id,use.rownames,html_class)
     dt_args <- list(
-        searching = FALSE,
-        scrollY = 250,
-        paging = TRUE,
-        scrollX = TRUE,
-        scrollCollapse = TRUE
+        searching=searching,
+        scrollY=scrollY,
+        paging=paging,
+        scrollX=scrollX,
+        scrollCollapse=scrollCollapse,
+        fixedColumns=fixedColumns,
+        ...
     )
     dt_args <- toJSON(dt_args,auto_unbox=TRUE)
     js <- sprintf(c(
