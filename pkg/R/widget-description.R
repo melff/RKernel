65e583203bed86a5539f9101c26901379012952f
Martin Elff <elff@mobitux11f.fritz.box>
2021-11-14 12:02:58 +0100

    Fix DescriptionStyle

--- pkg/R/widget-description.R
+++ pkg/R/widget-description.R
@@ -1,11 +1,15 @@
-#' @include widget.R widget-core.R
+#' @include widget.R widget-dom.R
 
 #' @export
 DescriptionStyleClass <- R6Class_("DescriptionStyle",
-  inherit = CoreWidgetClass,
+  inherit = WidgetClass,
   public = list(
+    `_model_module` = structure(Unicode("@jupyter-widgets/controls"),sync=TRUE),
+    `_model_module_version` = structure(Unicode(jupyter_widgets_controls_version),sync=TRUE),
     `_model_name` = structure(Unicode("DescriptionStyleModel"),sync=TRUE),
     `_view_name` = structure(Unicode("StyleView"),sync=TRUE),
+    `_view_module` = structure(Unicode("@jupyter-widgets/base"),sync=TRUE),
+    `_view_module_version` = structure(Unicode(jupyter_widgets_base_version),sync=TRUE),
     description_width = structure(Unicode(character(0)),sync=TRUE)
   )
 )
