3d9d8748448b82dbd25a3143ce49416f6571e532
Martin Elff <elff@mobitux11f.fritz.box>
2021-11-14 12:03:59 +0100

    Fix ButtonStyle

--- pkg/R/widget-button.R
+++ pkg/R/widget-button.R
@@ -17,17 +17,21 @@ ButtonStyleClass <- R6Class_("ButtonStyle",
 )
 
 #' @export
+ButtonStyle <- function(...) ButtonStyleClass$new(...)
+
+#' @export
 ButtonClass <- R6Class_("Button",
    inherit = DOMWidgetClass,
    public = list(
     `_model_name` = structure(Unicode("ButtonModel"),sync=TRUE),
     `_view_name` = structure(Unicode("ButtonView"),sync=TRUE),
-    description = structure(Unicode(),sync=TRUE),
+    description = structure(Unicode(""),sync=TRUE),
     disabled = structure(Boolean(FALSE),sync=TRUE),
-    icon = structure(Unicode(),sync=TRUE),
+    icon = structure(Unicode(""),sync=TRUE),
     button_style = structure(StrEnum(
         c("primary","success","info","warning","danger",""),
         default="")),
+    tooltip = structure(Unicode(""),sync=TRUE),
     style = structure(R6Instance(ButtonStyleClass),sync=TRUE),
     click_handlers = list(),
     initialize = function(...){
