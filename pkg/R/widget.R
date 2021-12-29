514572ce09dc84a9a3f5516ffc0837644827db03
Martin Elff <elff@newdebian.local>
2021-12-29 16:23:15 +0100

    Put CallbackDispatcherClass in its own file

--- pkg/R/widget.R
+++ pkg/R/widget.R
@@ -1,33 +1,4 @@
-#' @include json.R utils.R
-
-#' @export
-CallbackDispatcherClass <- R6Class("CallbackDispatcher",
-    public = list(
-      callbacks = list(),
-      register = function(handler,remove){
-        old <- self$callbacks
-        new <- list()
-        for(cb in old){
-          if(!identical(cb,handler))
-            new <- append(new,cb)
-        }
-        if(!remove){
-          new <- append(new,handler)
-        }
-        self$callbacks <- new
-      },
-      run = function(...){
-        # log_out(self$callbacks,use.print=TRUE)
-        for(cb in self$callbacks){
-            if(is.function(cb))cb(...)
-        }
-      }
-    )
-)
-
-
-#' @export
-CallbackDispatcher <- function(...) CallbackDispatcherClass$new(...)
+#' @include json.R utils.R callbacks.R
 
 #' @export
 WidgetClass <- R6Class_("Widget",
