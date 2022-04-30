7806ff772d0f7a5f516a7e0d92504f1cb809705d
Martin Elff <elff@newdebian.local>
2022-04-30 21:33:38 +0200

    Document the function 'View'

--- pkg/R/View.R
+++ pkg/R/View.R
@@ -1,3 +1,11 @@
+#' Invoke a Data Viewer
+#'
+#' This is a re-implementation of \code{\link[utils]{View}} that works within
+#' Jupyter notebooks by leveraging the \href{https://ipywidgets.readthedocs.io}{Jupyter widgets} infrastructure.
+#'
+#' @param x an \R object which can be coerced to a data frame with non-zero numbers of rows and columns.
+#' @param title a string used as title. Currently unused.
+#' 
 #' @export
 View <- function(x,
                  title=deparse(substitute(x)),
