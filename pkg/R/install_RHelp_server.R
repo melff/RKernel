d06f5304deeaa099c89b3f4aad31ed569b7138aa
Martin Elff <elff@newdebian.local>
2022-06-17 21:04:43 +0200

    python3 -m pip install instead of pip install

--- pkg/R/install_RHelp_server.R
+++ pkg/R/install_RHelp_server.R
@@ -5,7 +5,7 @@
 #' @export
 install_RHelp_server <- function(){
     RHelp_module_dir <- system.file("RHelp",package="RKernel")
-    RHelp_install_call <- paste("pip","install",RHelp_module_dir)
+    RHelp_install_call <- paste("python3","-m pip","install",RHelp_module_dir)
     exit_code <- system2(RHelp_install_call)
     invisible(exit_code)
 }
