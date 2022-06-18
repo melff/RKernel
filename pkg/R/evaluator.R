6e8e410e98b9263487765a402a4af9433a7e3942
Martin Elff <elff@newdebian.local>
2022-06-18 17:36:23 +0200

    Make RHelp work again in the non-proxy case

--- pkg/R/evaluator.R
+++ pkg/R/evaluator.R
@@ -341,7 +341,8 @@ Evaluator <- R6Class("Evaluator",
             assign("get_help_url",function()private$help_url,envir=private$env)
         },
         start_proxied_help_system = function(help_url=paste0(Sys.getenv("JUPYTERHUB_SERVICE_PREFIX"),
-                                                           "RHelp")){
+                                                           "/RHelp")){
+            help_url <- gsub("//","/",help_url,fixed=TRUE)
             private$help_url <- help_url
             assign("get_help_url",function()private$help_url,envir=private$env)
         },
