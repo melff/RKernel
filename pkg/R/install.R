f230e38cfcf34246aa7ce0482560bd9e13bdb9df
Martin Elff <elff@newdebian.local>
2022-06-18 17:40:05 +0200

    Provide 'install' function, which installs both kernel spec and RHelp package

--- pkg/R/install.R
+++ pkg/R/install.R
@@ -1,6 +1,13 @@
+#' install the R Kernel
+#'
+#' @export
+install <- function(){
+    installspec()
+    install_RHelp_server()
+}
 
 
-
+#' @rdname install
 #' Install the R Kernel spec
 #'
 #' @importFrom jsonlite fromJSON toJSON
@@ -29,4 +36,17 @@ installspec <- function(){
    exit_code <- system2(jupyter_call)
    unlink(tmp_dir,recursive=TRUE)
    invisible(exit_code)
+}
+
+#' @rdname install
+#' Install an RHelp server module
+#'
+#' @description Install a Python module that provides for an R process being
+#'     started when needed to display R help pages.
+#' @export
+install_RHelp_server <- function(){
+    RHelp_module_dir <- system.file("RHelp",package="RKernel")
+    RHelp_install_call <- paste("python3","-m pip","install",RHelp_module_dir)
+    exit_code <- system(RHelp_install_call)
+    invisible(exit_code)
 }
