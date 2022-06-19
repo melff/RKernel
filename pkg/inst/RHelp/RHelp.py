8d741746d994d40fda4ebe0bc6e0c7ce1a4e8d6c
Martin Elff <elff@newdebian.local>
2022-06-19 02:41:01 +0200

    Disable global RHelp menu entry

--- pkg/inst/RHelp/RHelp.py
+++ pkg/inst/RHelp/RHelp.py
@@ -2,5 +2,8 @@ def setup_RHelp():
     return {
         'command': ['R', '-q', 
                     '-e','options(help.ports={port}); tools::startDynamicHelp(); repeat Sys.sleep(60)'],
-        'absolute_url': False
+        'absolute_url': False,
+        'launcher_entry' : {
+            'enabled': False
+        }
     }
