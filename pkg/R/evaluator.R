7cb9555098887e0ba46873607e124b669f7b7bc0
Martin Elff <elff@mobitux11f.fritz.box>
2022-08-07 21:30:39 +0100

    evaluator.R: source RKernel-startup.R or .RKernel-profile in any parent directory

--- pkg/R/evaluator.R
+++ pkg/R/evaluator.R
@@ -95,9 +95,20 @@ Evaluator <- R6Class("Evaluator",
             jupyter_config <- file.path(home_dir,".jupyter","RKernel-config.R")
             if(file.exists(jupyter_config)) 
                 source(jupyter_config)
-            if(file.exists("RKernel-startup.R"))
-                source("RKernel-startup.R")
+            #if(file.exists("RKernel-startup.R"))
+            #    source("RKernel-startup.R")
 
+            parents <- strsplit(getwd(),.Platform$file.sep)[[1]]
+            parents <- Reduce(file.path,parents,accumulate=TRUE)
+            for(parent in parents){
+                dot_file <- file.path(parent,".RKernel-profile")
+                if(file.exists(dot_file))
+                    source(dot_file)
+                startup_file <- file.path(parent,"RKernel-startup.R")
+                if(file.exists(startup_file))
+                    source(startup_file)
+            }
+            
             private$start_help_system()
             assign("help.start",help.start,envir=private$env)
         },
