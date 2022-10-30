ef21d570b51b325d4f4f563b171e1bff61ff6bce
Martin Elff <elff@newdebian.local>
2022-10-30 21:00:22 +0100

    Use proper file extensions with dots for graphics

--- pkg/R/graphics.R
+++ pkg/R/graphics.R
@@ -2,7 +2,7 @@
 mime_graphics <- function(plt,mime,width,height,pointsize,scale,res,units="in"){
     grapics_data <- NULL
     if(mime=="image/png"){
-        tf <- tempfile(fileext="png")
+        tf <- tempfile(fileext=".png")
         png(file=tf,
             width=width,
             height=height,
@@ -13,7 +13,7 @@ mime_graphics <- function(plt,mime,width,height,pointsize,scale,res,units="in"){
         graphics_data <- readBin(tf, raw(), file.info(tf)$size)
     }
     else if(mime=="image/jpeg"){
-        tf <- tempfile(fileext="jpeg")
+        tf <- tempfile(fileext=".jpeg")
         jpeg(file=tf,
             width=width,
             height=height,
@@ -24,7 +24,7 @@ mime_graphics <- function(plt,mime,width,height,pointsize,scale,res,units="in"){
         graphics_data <- readBin(tf, raw(), file.info(tf)$size)
     }
     else if(mime=="image/svg+xml"){
-        tf <- tempfile(fileext="svg")
+        tf <- tempfile(fileext=".svg")
         svg(file=tf,
             width=width,
             height=height)
@@ -33,7 +33,7 @@ mime_graphics <- function(plt,mime,width,height,pointsize,scale,res,units="in"){
         graphics_data <- readChar(tf, file.info(tf)$size,useBytes=TRUE)
     }
     else if(mime=="application/pdf"){
-        tf <- tempfile(fileext="pdf")
+        tf <- tempfile(fileext=".pdf")
         pdf(file=tf,
             width=width,
             height=height)
