596273972373820ad59cd57e4756ba081be473b4
Martin Elff <elff@newdebian.local>
2022-11-10 14:13:42 +0100

    SVG plotting widgets based on svglite

--- pkg/R/widget-plot.R
+++ pkg/R/widget-plot.R
@@ -86,3 +86,94 @@ PlotWidget <- function(...) PlotWidgetClass$new(...)
 #' @export
 with.PlotWidget <- function(data,expr,enclos=parent.frame(),...)
     data$context$eval(substitute(expr),enclos=enclos)
+
+
+
+
+
+#' Widgets to receive plots graphics
+#' @description Classes and constructors show graphics created by code
+#' @include widget-media.R
+#' @name SVGWidget
+
+#' @rdname SVGWidget
+#' @importFrom svglite svgstring
+#' @export
+SVGWidgetClass <- R6Class_(
+    "SVGWidget",
+    inherit = HTMLClass,
+    public = list(
+        context = NULL,
+        initialize = function(...,
+                              envir = new.env()){
+            super$initialize(...)
+            context <- Context$new(envir=self$envir,
+                                   attachment=list(
+                                       display=self$display
+                                   ))
+            context$on_enter(self$enter)
+            context$on_exit(self$exit)
+
+            context$on_eval(self$handle_graphics)
+            context$on_result(self$handle_graphics)
+            self$context <- context
+            private$kernel <- get_current_kernel()
+        },
+        handle_graphics = function(){
+
+            plt <- self$context$get_graphics()
+            if(!length(plt)) return(NULL)
+            
+            width <- getOption("jupyter.plot.width",6)
+            height <- getOption("jupyter.plot.height",6)
+            pointsize <- getOption("jupyter.plot.pointsize",12)
+            scale <- getOption("jupyter.plot.scale",0.5)
+
+            self$value <- private$get_svg(plt,
+                                          width=width,
+                                          height=height,
+                                          pointsize=pointsize)
+        },
+        enter = function(){
+            parent <- private$kernel$get_parent("shell")
+            self$msg_id <- parent$header$msg_id
+            graphics$current$push(self$context$current_plot)
+            em <- get_current_event_manager()
+            for(event in c("before_print","print","before_cat","cat")){
+                em$push_handlers(event)
+            }
+        },
+        exit = function(){
+            self$msg_id <- ""
+            self$context$current_plot <- graphics$current$pop()
+            em <- get_current_event_manager()
+            for(event in c("before_print","print","before_cat","cat")){
+                em$pop_handlers(event)
+            }
+        }
+    ),
+    private = list(
+        kernel = NULL,
+        get_svg = function(plt,width,height,pointsize){
+            svgstr <- svgstring(width=width,height=height,pointsize=pointsize,
+                                standalone=FALSE)
+            replayPlot(plt)
+            dev.off()
+            return(svgstr())
+        }
+    )
+)
+
+#' @rdname SVGWidget
+#' @param ... Arguments, passed to the ImageWidget constructors.
+#' @export
+SVGWidget <- function(...) SVGWidgetClass$new(...)
+
+#' @rdname SVGWidget
+#' @param data An "SVGWidget" object
+#' @param expr An expression to evaluate, or a sequence of expression, 
+#'    encapsulated by curly braces.
+#' @param enclos An enclosing environment.
+#' @export
+with.SVGWidget <- function(data,expr,enclos=parent.frame(),...)
+    data$context$eval(substitute(expr),enclos=enclos)
