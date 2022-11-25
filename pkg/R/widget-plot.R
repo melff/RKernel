7a842bc780ab77200b1f63a59e4d90c7713d7376
Martin Elff <elff@newdebian.local>
2022-11-26 00:39:33 +0100

    Make plotWidget and SVGWidget synonymous

--- pkg/R/widget-plot.R
+++ pkg/R/widget-plot.R
@@ -1,96 +1,10 @@
 #' Widgets to receive plots graphics
-#' @description Classes and constructors show graphics created by code
+#' @description Class and constructors show graphics created by code
 #' @include widget-media.R
 #' @name PlotWidget
 
-#' @rdname PlotWidget
-#' @export
-PlotWidgetClass <- R6Class_(
-    "PlotWidget",
-    inherit = ImageWidgetClass,
-    public = list(
-        context = NULL,
-        graphics = NULL,
-        initialize = function(...,
-                              envir = new.env()){
-            super$initialize(...)
-            context <- Context$new(envir=self$envir,
-                                   attachment=list(
-                                       display=self$display
-                                   ))
-            context$on_enter(self$enter)
-            context$on_exit(self$exit)
-
-            context$on_eval(self$handle_graphics)
-            self$context <- context
-            self$value <- empty20x20
-            private$kernel <- get_current_kernel()
-            self$graphics <- GraphicsDevice$new()
-        },
-        handle_graphics = function(){
-
-            if(!self$graphics$is_active()) return(NULL)
-            plt <- self$graphics$get_plot()
-            if(!length(plt)) return(NULL)
-            
-            width      <- getOption("jupyter.plot.width",6)
-            height     <- getOption("jupyter.plot.height",6)
-            pointsize  <- getOption("jupyter.plot.pointsize",12)
-            res        <- getOption("jupyter.plot.res",150)
-            scale      <- getOption("jupyter.plot.scale",0.5)
-            units      <- getOption("jupyter.plot.units","units")
-
-            self$width <- width*res*scale
-            self$height <- height*res*scale
-            
-            self$value <- mime_graphics(plt,
-                                        mime="image/png",
-                                        width=width,
-                                        height=height,
-                                        pointsize=pointsize,
-                                        scale=scale,
-                                        res=res,
-                                        units=units)
-        },
-        enter = function(){
-            parent <- private$kernel$get_parent("shell")
-            self$msg_id <- parent$header$msg_id
-            self$graphics$activate()
-        },
-        exit = function(){
-            self$msg_id <- ""
-            self$graphics$suspend()
-        }
-    ),
-    private = list(
-        kernel = NULL
-    )
-)
 
 #' @rdname PlotWidget
-#' @param ... Arguments, passed to the ImageWidget constructors.
-#' @export
-PlotWidget <- function(...) PlotWidgetClass$new(...)
-
-#' @rdname PlotWidget
-#' @param data An "PlotWidget" object
-#' @param expr An expression to evaluate, or a sequence of expression, 
-#'    encapsulated by curly braces.
-#' @param enclos An enclosing environment.
-#' @export
-with.PlotWidget <- function(data,expr,enclos=parent.frame(),...)
-    data$context$eval(substitute(expr),enclos=enclos)
-
-
-
-
-
-#' Widgets to receive plots graphics
-#' @description Classes and constructors show graphics created by code
-#' @include widget-media.R
-#' @name SVGWidget
-
-#' @rdname SVGWidget
 #' @importFrom svglite svgstring
 #' @export
 SVGWidgetClass <- R6Class_(
@@ -170,12 +84,12 @@ SVGWidgetClass <- R6Class_(
     )
 )
 
-#' @rdname SVGWidget
+#' @rdname PlotWidget
 #' @param ... Arguments, passed to the ImageWidget constructors.
 #' @export
 SVGWidget <- function(...) SVGWidgetClass$new(...)
 
-#' @rdname SVGWidget
+#' @rdname PlotWidget
 #' @param data An "SVGWidget" object
 #' @param expr An expression to evaluate, or a sequence of expression, 
 #'    encapsulated by curly braces.
@@ -183,3 +97,10 @@ SVGWidget <- function(...) SVGWidgetClass$new(...)
 #' @export
 with.SVGWidget <- function(data,expr,enclos=parent.frame(),...)
     data$context$eval(substitute(expr),enclos=enclos)
+
+
+#' @rdname PlotWidget
+#' @param ... Arguments, passed to the ImageWidget constructors.
+#' @export
+PlotWidget <- function(...) SVGWidgetClass$new(...)
+
