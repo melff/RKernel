474ebeae883cdc3460dd21055ac7282ba60d7795
Martin Elff <elff@newdebian.local>
2022-12-17 20:16:00 +0100

    Properly export 'dump.frames'

--- pkg/R/evaluator.R
+++ pkg/R/evaluator.R
@@ -127,6 +127,7 @@ Evaluator <- R6Class("Evaluator",
             assign("readline",self$readline,envir=private$env)
             assign("envBrowser",envBrowser,envir=private$env)
             assign("str",self$str,envir=private$env)
+            assign("dump.frames",dump.frames,envir=private$env)
             # log_out("evaluator$startup() complete")
             em <- EventManager(type="eval")
             em$activate()
