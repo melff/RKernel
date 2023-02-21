52f218ac6cff4adddea9c5800de6d090454c1301
Martin Elff <elff@mobitux11f.fritz.box>
2023-02-21 22:38:50 +0100

    Add documentation to 'traitlets-bytes.R'

--- pkg/R/traitlets-bytes.R
+++ pkg/R/traitlets-bytes.R
@@ -1,12 +1,25 @@
-#' @include traitlets.R
+#' Raw Bytes Traitlets
 #'
+#' @description A class and a constructor function to create (raw) bytes trait(let)s.
+#'
+#' @include traitlets.R
+#' @name Bytes
+
+#' @rdname Bytes
 #' @export
 BytesClass <- R6Class_("Bytes",
     inherit=TraitClass,
     public=list(
+        #' @field value A raw bytes vector
         value = raw(0),
+        #' @field optional Logical, whether an initializing logical value must be provided
         optional = TRUE,
+        #' @field coerce Logical, whether 'as.raw()' is implicitely used when a value is
+        #'   assigned to the trait
         coerce = TRUE,
+        #' @description
+        #' A validator method
+        #' @param value A value to be checked for validity
         validator=function(value){
             if(self$coerce){
                 value <- as.raw(value)
@@ -21,6 +34,11 @@ BytesClass <- R6Class_("Bytes",
             }
             value
         },
+        #' @description
+        #' The initializing method
+        #' @param initial A value with which the traitlet is initialized
+        #' @param coerce Logical, used to initialize the 'coerce' field
+        #' @param optional Logical, used to initialize the 'optional' field
         initialize=function(initial=raw(0),coerce=TRUE,
                             optional=TRUE
                             ){
@@ -29,9 +47,12 @@ BytesClass <- R6Class_("Bytes",
             super$initialize(initial)
 }))
 
+#' @rdname Bytes
+#' @param ... Arguments that are passed to the initialize method of 'BytesClass'
 #' @export
 Bytes <- function(...)TraitInstance(...,Class=BytesClass)
 
+#' @describeIn to_json S3 method for 'BytesClass' objects
 #' @export
 to_json.Bytes <- function(x,...){
     #value <- x$get()
