2d61fe16e139558f26b4b0299369e27eabe1a32a
Martin Elff <elff@newdebian.local>
2023-02-25 00:10:54 +0100

    Complete documentation in 'widget-integer.R'

--- pkg/R/widget-integer.R
+++ pkg/R/widget-integer.R
@@ -1,8 +1,8 @@
 #' Widgets for Integer Numbers
 #' @description An R6 class and a constructor function for the creation of
-#'     widgets that can be used to manipulate integer numbers
+#'     widgets that can be used to manipulate integer numbers.
 #' @details The function \code{IntWidget} creates objects of the R6 Class
-#'     "IntWidgetClass", which in turn have the S3 class attribute "IntWidget"
+#'     "IntWidgetClass", which in turn have the S3 class attribute "IntWidget".
 #' @include widget-value.R
 #' @name IntWidget
 
@@ -11,21 +11,22 @@
 IntWidgetClass <- R6Class_("IntWidget",
   inherit = ValueWidgetClass,
   public = list(
-      value = structure(Integer(0L),sync=TRUE)
+    #' @field value A \link{Integer} traitlet.
+    value = structure(Integer(0L),sync=TRUE)
   )
 )
 
 #' @rdname IntWidget
-#' @param value The integer value
-#' @param ... Other arguments, passed to the superclass initializer
+#' @param value The integer value.
+#' @param ... Other arguments, passed to the superclass initializer.
 #' @export
 IntWidget <- function(value,...) IntWidgetClass$new(value=value,...)
 
 #' Widgets for Bounded Integer Numbers
 #' @description An R6 class and a constructor function for the creation of
-#'     widgets that can be used to manipulate integer numbers that are bounded within an interval
+#'     widgets that can be used to manipulate integer numbers that are bounded within an interval.
 #' @details The function \code{BoundedIntWidget} creates objects of the R6 Class
-#'     "BoundedIntWidgetClass", which in turn have the S3 class attribute "BoundedIntWidget"
+#'     "BoundedIntWidgetClass", which in turn have the S3 class attribute "BoundedIntWidget".
 #' @name BounedIntWidget
 NULL
 
@@ -34,41 +35,51 @@ NULL
 BoundedIntWidgetClass <- R6Class_("BoundedIntWidget",
   inherit = ValueWidgetClass,
   public = list(
-      value = structure(Integer(0L),sync=TRUE),
-      min = structure(Integer(0L),sync=TRUE),
-      max = structure(Integer(100L),sync=TRUE),
-      validate_value = function(value){
-          if(value > self$max)
-              value <- self$max
-          if(value < self$min)
-              value <- self$min
-          value
-      },
-      validate_min = function(min){
-          if(min > self$max) stop("min <= max required")
-          if(min > self$value)
-              self$value <- min
-          min
-      },
-      validate_max = function(max){
-          if(max < self$min) stop("min <= max required")
-          if(max < self$value)
-              self$value <- max
-          max
-      },
-      initialize = function(...){
-          super$initialize(...)
-          self$validate("value",self$validate_value)
-          self$validate("min",self$validate_min)
-          self$validate("max",self$validate_max)
-      })
+    #' @field value A \link{Integer} traitlet.
+    value = structure(Integer(0L),sync=TRUE),
+    #' @field min A \link{Integer} traitlet, the minimum allowed value.
+    min = structure(Integer(0L),sync=TRUE),
+    #' @field max A \link{Integer} traitlet, the maximum allowed value.
+    max = structure(Integer(100L),sync=TRUE),
+    #' @description Validate the "value" after assignment.
+    #' @param value A value, should be an integer number.
+    validate_value = function(value){
+        if(value > self$max)
+            value <- self$max
+        if(value < self$min)
+            value <- self$min
+        value
+    },
+    #' @description Validate the "min" field after assignment.
+    #' @param min A minimum value, should be an integer number.
+    validate_min = function(min){
+        if(min > self$max) stop("min <= max required")
+        if(min > self$value)
+            self$value <- min
+        min
+    },
+    #' @description Validate the "max" field after assignment.
+    #' @param max A maximum value, should be an integer number.
+    validate_max = function(max){
+        if(max < self$min) stop("min <= max required")
+        if(max < self$value)
+            self$value <- max
+        max
+    },
+    #' @param ... Arguments passed to the superclass initializer
+    initialize = function(...){
+        super$initialize(...)
+        self$validate("value",self$validate_value)
+        self$validate("min",self$validate_min)
+        self$validate("max",self$validate_max)
+    })
   )
 
 #' @rdname BoundedIntWidget
-#' @param value The integer value
-#' @param min The lower bound of the interval
-#' @param max The upper bound of the interval
-#' @param ... Other arguments, passed to the superclass initializer
+#' @param value The integer value.
+#' @param min The lower bound of the interval.
+#' @param max The upper bound of the interval.
+#' @param ... Other arguments, passed to the superclass initializer.
 #' @export
 BoundedIntWidget <- function(value,min,max,...) 
     BoundedIntWidgetClass$new(value=value,
@@ -77,17 +88,18 @@ BoundedIntWidget <- function(value,min,max,...)
                                   ...)
 
 #' @rdname BoundedIntRangeWidget
-#' @param value A pair of integer values
-#' @param min The lower bound of the enclosing interval
-#' @param max The upper bound of the enclosing interval
-#' @param ... Other arguments, passed to the superclass initializer
 #' @export
 BoundedIntRangeWidgetClass <- R6Class_("BoundedIntRangeWidget",
   inherit = ValueWidgetClass,
   public = list(
+      #' @field value A \link{Integer} traitlet.
       value = structure(Integer(c(0L,1L)),sync=TRUE),
+      #' @field min A \link{Integer} traitlet, the minimum allowed value.
       min = structure(Integer(0L),sync=TRUE),
+      #' @field max A \link{Integer} traitlet, the maximum allowed value.
       max = structure(Integer(100L),sync=TRUE),
+      #' @description Validate the "value" after assignment.
+      #' @param value A value, should be an integer number.
       validate_value = function(value){
           if(length(value) > 2)
               value <- value[1:2]
@@ -100,6 +112,8 @@ BoundedIntRangeWidgetClass <- R6Class_("BoundedIntRangeWidget",
               value[1] <- self$min
           value
       },
+      #' @description Validate the "min" field after assignment.
+      #' @param min A minimum value, should be an integer number.
       validate_min = function(min){
           if(min > self$max) stop("min <= max required")
           if(min > self$value[1])
@@ -108,6 +122,8 @@ BoundedIntRangeWidgetClass <- R6Class_("BoundedIntRangeWidget",
               self$value[2] <- min
           min
       },
+      #' @description Validate the "max" field after assignment.
+      #' @param max A maximum value, should be an integer number.
       validate_max = function(max){
           if(max < self$min) stop("min <= max required")
           if(max < self$value[2])
@@ -116,6 +132,7 @@ BoundedIntRangeWidgetClass <- R6Class_("BoundedIntRangeWidget",
               self$value[1] <- max
           max
       },
+      #' @param ... Arguments passed to the superclass initializer.
       initialize = function(...){
           super$initialize(...)
           self$validate("value",self$validate_value)
@@ -148,10 +165,17 @@ BoundedIntRangeWidget <- function(value,min,max,...)
 IntTextClass <- R6Class_("IntText",
    inherit = IntWidgetClass,
    public = list(
+       #' @field _model_name Name of the Javascript model in the frontend.
        `_model_name` = structure(Unicode("IntTextModel"),sync=TRUE),
+       #' @field _view_name Name of the Javascript view in the frontend.
        `_view_name` = structure(Unicode("IntTextView"),sync=TRUE),
+       #' @field A \link{Boolean} traitlet, whether the text widget is disabled.
        disabled = structure(Boolean(FALSE),sync=TRUE),
+       #' @field A \link{Boolean} traitlet, whether the text widget is
+       #'    continuously updated upon change in the frontend.
        continuous_update = structure(Boolean(FALSE),sync=TRUE),
+       #' @field An \link{Integer} traitlet, a step size by which the 
+       #'   value is incremented or decremented if the arrows are clicked.
        step = structure(Integer(1L),sync=TRUE)
    ))
 
@@ -174,10 +198,17 @@ IntText <- function(value=0,step=1,...) IntTextClass$new(value=value,step=step,.
 BoundedIntTextClass <- R6Class_("BoundedIntText",
    inherit = BoundedIntWidgetClass,
    public = list(
+       #' @field _model_name Name of the Javascript model in the frontend.
        `_model_name` = structure(Unicode("BoundedIntTextModel"),sync=TRUE),
+       #' @field _view_name Name of the Javascript view in the frontend.
        `_view_name` = structure(Unicode("IntTextView"),sync=TRUE),
+       #' @field A \link{Boolean} traitlet, whether the text widget is disabled.
        disabled = structure(Boolean(FALSE),sync=TRUE),
+       #' @field A \link{Boolean} traitlet, whether the text widget is
+       #'    continuously updated upon change in the frontend.
        continuous_update = structure(Boolean(FALSE),sync=TRUE),
+       #' @field A \link{Integer} traitlet, a step size by which the 
+       #'   value is incremented or decremented if the arrows are clicked.
        step = structure(Integer(1L),sync=TRUE)
    ))
 
