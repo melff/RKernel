d32b6b7ebf48a3fe0742435bda8d3645bef1886c
Martin Elff <elff@newdebian.local>
2023-02-25 21:53:43 +0100

    Complete documentation in 'widget-media.R'

--- pkg/R/widget-media.R
+++ pkg/R/widget-media.R
@@ -8,11 +8,20 @@
 MediaWidgetClass <- R6Class_("MediaWidget",
     inherit = DOMWidgetClass,
     public = list(
+      #' @field _model_name Name of the Javascript model in the frontend.
       `_model_module` = structure(Unicode("@jupyter-widgets/controls"),sync=TRUE),
+      #' @field _model_module_version Version of the Javascript model module in the frontend.
       `_model_module_version` = structure(Unicode(jupyter_widgets_controls_version),sync=TRUE),
+      #' @field _view_module_version Version of the Javascript view module in the frontend.
       `_view_module` = structure(Unicode("@jupyter-widgets/controls"),sync=TRUE),
+      #' @field _view_module_version Version of the Javascript view module in the frontend.
       `_view_module_version` = structure(Unicode(jupyter_widgets_controls_version),sync=TRUE),
+      #' @field value A \link{Bytes} traitlet.
       value = structure(Bytes(),sync=TRUE),
+      #' @description Add or remove a handler to be called if value 
+      #'    is changed.
+      #' @param handler A function that is called when the button is clicked.
+      #' @param remove Logical value, whether the handler is to be removed.
       on_change = function(handler,remove=FALSE){
           self$observe("value",handler,remove)
       }
@@ -24,10 +33,15 @@ MediaWidgetClass <- R6Class_("MediaWidget",
 ImageWidgetClass <- R6Class_("ImageWidget",
     inherit = MediaWidgetClass,
     public =list(
+        #' @field _model_name Name of the Javascript view in the frontend.
         `_view_name` = structure(Unicode("ImageView"),sync=TRUE),
+        #' @field _model_name Name of the Javascript model in the frontend.
         `_model_name` = structure(Unicode("ImageModel"),sync=TRUE),
+        #' @field format A string, giving the graphics fromat.
         format = structure(Unicode("png"),sync=TRUE),
+        #' @field width A string, describing the width in CSS language, e.g. "480px".
         width = structure(Unicode(""),sync=TRUE),
+        #' @field width A string, describing the height in CSS language, e.g. "480px".
         height = structure(Unicode(""),sync=TRUE)
     )
 )
