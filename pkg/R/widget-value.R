be1114a7442ce95dd1412287e46d19990efa7d30
Martin Elff <elff@newdebian.local>
2023-05-14 01:10:33 +0200

    Add documentation for value widgets

--- pkg/R/widget-value.R
+++ pkg/R/widget-value.R
@@ -1,15 +1,25 @@
+#' Value widgets
+#'
+#' @description A base class for widgets that are connected with values
 #' @include widget-description.R
-
 #' @export
 ValueWidgetClass <- R6Class_("ValueWidget",
   inherit = DescriptionWidgetClass,
   public = list(
+      #' @field value A list or any other vector of values
       value = structure(list(),sync=TRUE),
+      #' @description A generic initializer function
+      #' @param value A value to initialize instance with
+      #' @param ... Any other arguments, ignored.
       initialize = function(value,...){
           super$initialize(...)
           if(!missing(value))
               self$value <- value
       },
+      #' @description Add handler function to be called when value is changed
+      #' @param handler A handler function
+      #' @param remove A logical value, whether the handler should be removed
+      #'    or added.
       on_change = function(handler,remove=FALSE){
           self$observe("value",handler,remove)
       }
