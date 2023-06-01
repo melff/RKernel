1f78cc0186787f598c306a509f955a438f2977ff
Martin Elff <elff@mobitux11f.fritz.box>
2023-06-01 08:49:59 +0200

    Export and add documententation for EventManager class

--- pkg/R/events.R
+++ pkg/R/events.R
@@ -1,8 +1,23 @@
+#' A Manager for Comms
+#'
+#' @description Objects of this class are used internally to manage events, they
+#'     are not meant to be used by end-users.
+#' @name EventManager
+NULL
+
+#' @rdname EventManager
+#' @export
 EventManagerClass <- R6Class("EventManager",
  public = list(
+     #' @description 
+     #' Initialize an event manager
+     #' @param type A string, the type of event (e.g. "print")
      initialize = function(type){
          private$type = type
      },
+     #' @description Send an event
+     #' @param event A string, the name of an event
+     #' @param ... Other arguments, sent to the event handler(s)
      send = function(event,...){
          # log_out(sprintf("eventmanager$send(\"%s\",...)",event))
          # log_out(private$dispatchers,use.str=TRUE)
@@ -16,6 +31,10 @@ EventManagerClass <- R6Class("EventManager",
              }
          }
       },
+     #' @description Install a handler for an event
+     #' @param event A string, the name of an event
+     #' @param handler A function 
+     #' @param remove A logical value, whether the handler is to be removed
      on = function(event,handler,remove=FALSE){
          # log_out(sprintf("eventmanager$on(\"%s\",...)",event))
          ed <- private$dispatchers[[event]]
@@ -28,6 +47,10 @@ EventManagerClass <- R6Class("EventManager",
              # log_out(sprintf("Adding handler for %s",event))
          ed$register(handler=handler,remove=remove)
      },
+     #' @description Activate handlers
+     #' @param event A string, the name of an event, ignored if 'all' is TRUE.
+     #' @param all A logical value, if TRUE, all handlers that belong to 
+     #'   the event type of the event manager are activated.
      activate = function(event=NULL,all=TRUE){
          if(all){
              if(!private$active){
@@ -49,6 +72,10 @@ EventManagerClass <- R6Class("EventManager",
              }
          }
      },
+     #' @description Suspend handlers
+     #' @param event A string, the name of an event, ignored if 'all' is TRUE.
+     #' @param all A logical value, if TRUE, all handlers that belong to 
+     #'   the event type of the event manager are suspended.
      suspend = function(event=NULL,all=TRUE){
          if(all && private$active){
              # log_out(sprintf("Activating event manager of type '%s",private$type))
@@ -65,6 +92,8 @@ EventManagerClass <- R6Class("EventManager",
                  ed$suspend_handlers()
          }
      },
+     #' @description Clear (i.e. remove) handlers for an event
+     #' @param event A string, the name of an event
      clear = function(event){
          if(private$active && length(event) && event %in% names(private$dispatchers)){
              ed <- private$dispatchers[[event]]
@@ -72,9 +101,13 @@ EventManagerClass <- R6Class("EventManager",
                  ed$clear()
          }
      },
+     #' @description Resume (i.e. reactivate) an event handler
      resume = function(){
          private$active <- TRUE
      },
+     #' @description Check whether the event manager has handlers for the
+     #'   given events
+     #' @param events A character vector with names of events
      has = function(events){
          if(length(private$dispatchers))
              all(events %in% names(private$dispatchers))
@@ -89,6 +122,9 @@ EventManagerClass <- R6Class("EventManager",
  )
 )
 
+#' @describeIn EventManager The constructor function, returns an Object of Class "EventManagerClass" 
+#' @param ... Arguments passed to the inializer
+#' @export
 EventManager <- function(...) EventManagerClass$new(...)
 
 eventmanagers <- new.env()
