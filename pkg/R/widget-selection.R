3362e5c70c3558e2fe934f326e8737ebb59a49c8
Martin Elff <elff@newdebian.local>
2023-09-16 00:32:10 +0200

    Refactored Selection and MultipleSelection widgets.

--- pkg/R/widget-selection.R
+++ pkg/R/widget-selection.R
@@ -6,119 +6,62 @@
 #' @rdname SelectionWidget
 #' @export
 SelectionWidgetClass <- R6Class_("SelectionWidget",
-  inherit = ValueWidgetClass,
+  inherit = DescriptionWidgetClass,
   public = list(
       #' @field _options_labels A unicode vector of option labels.
       `_options_labels` = structure(Unicode(length=NA),sync=TRUE),
-      #' @field _options_values A list of values - the options.
-      `_options_values` = list(),
       #' @field index An integer that refers to currently selected item.
       index = structure(Integer(0L,optional=TRUE,length=1L),sync=TRUE),
-      #' @field value Any kind of trait(let).
-      value = Trait(),
       #' @description Validate an index argument.
       #' @param index The index to be checked.
       validate_index = function(index){
-          # cat("validate_index")
-          options_values <- self[["_options_values"]]
-          len <- length(options_values)
+          # log_out("validate_index")
+          options_labels <- self[["_options_labels"]]
+          len <- length(options_labels)
           if(length(index) > 0){
+              index <- index[1]
               if(index >= len)
                   index <- len - 1L
               if(index < 0)
                   index <- 0L
           } 
+          else index <- self$index
           index
-      },
-      #' @description Standard observer function for indices
-      #' @param name Name of the traitlet
-      #' @param self The traitlet container
-      #' @param index The index
-      observe_index = function(name,self,index){
-          #cat("observe_index")
-          options_values <- self[["_options_values"]]
-          if(length(index) > 0){
-              index <- index[1]
-              value <- options_values[[index + 1L]]
-          } else {
-              value <- NULL
-          }
-          self$traits$value$set(value,notify=FALSE)
-          # cat(value)
-          self$send_state()
-      },
-      #' @description Validate a value
-      #' @param value The value to be validated
-      validate_value = function(value){
-          # cat("validate_value")
-          options_labels <- self[["_options_labels"]]
-          options_values <- self[["_options_values"]]
-          if(length(value) > 1)
-              value <- value[1]
-          if(length(value) == 0){
-              value <- NULL
-          }
-          else if(is.character(value) && value %in% options_labels){
-              i <- match(value,options_labels)
-              value <- options_values[i]
-          }
-          else if(!(value %in% options_values)){
-              value <- self$value
-          }
-          value
-      },
-      #' @description Standard observer function for values
-      #' @param name Name of the traitlet
-      #' @param self The traitlet container
-      #' @param value A value
-      observe_value = function(name,self,value){
-          # cat("observe_value\n")
-          # print(name)
-          # cat("value\n")
-          # print(value)
-          if(length(value)){
-              options_labels <- self[["_options_labels"]]
-              options_values <- self[["_options_values"]]
-              if(value %in% options_labels){
-                  index <- match(value,options_labels) - 1L
-              } 
-              else if(value %in% options_values){
-                  index <- match(value,options_values) - 1L
-              }
-          }
-          else 
-              index <- integer(0)
-          self$traits$index$set(index,notify=FALSE)
-          # str(self)
-          self$send_state()
       },
       #' @description Initialiser
       #' @param options A named vector or a vector coerceable into a character vector.
-      #' @param value A trait.
+      #' @param value Name of a selectable option.
       #' @param ... Any other arguments, ignored.
       initialize = function(options,value,...){
-          # print(options)
-          # args <- list(...)
-          # print(args)
           super$initialize(...)
-          if(length(names(options))) {
-              options <- names(options)
-              self[["_options_labels"]] <- options
-          }
-          else {
-              options <- as.character(options)
-              self[["_options_labels"]] <- options
-          }
-          self[["_options_values"]] <- options
+          options <- as.character(options)
+          self[["_options_labels"]] <- options
           self$validate("index",self$validate_index)
-          self$validate("value",self$validate_value)
-          self$observe("value",self$observe_value)
-          self$observe("index",self$observe_index)
           if(!missing(value)){
-              self$value <- value
+              value <- value[1L]
+              index <- match(value,options,nomatch=0L)
+              index <- index[index > 0L]
+              if(length(index))
+                  self$index <- index - 1L
           }
-          else
-              self$value <- options[[1]]
+      }
+  ),
+  active = list(
+      #' @field value The selected option
+      value = function(value){
+          if(missing(value)){
+              index <- self$traits$index$value + 1L
+              options_labels <- self[["_options_labels"]]
+              options_labels[index]
+          } else {
+              options_labels <- self[["_options_labels"]]
+              index <- match(value,options_labels,nomatch=0L)
+              index <- index[index > 0L]
+              if(length(index)){
+                  self$traits$index$set(index[1L] - 1L)
+                  self$send_state()
+              }
+          }
       }
   )
 )
@@ -280,112 +223,55 @@ MultipleSelectionWidgetClass <- R6Class_("MultipleSelectionWidget",
   public = list(
       #' @field _options_labels A unicode string vector with labels
       `_options_labels` = structure(Unicode(length=NA),sync=TRUE),
-      #' @field _options_values A list of labelled values
-      `_options_values` = list(),
       #' @field index An integer vector of indices of currenlty selected elements.
       index = structure(Integer(integer(0),optional=TRUE,length=NA),sync=TRUE),
-      #' @field value A traitlet vector.
-      value = Trait(),
       #' @description Validate an index.
       #' @param index An index, the index to be checked.
       validate_index = function(index){
-          # cat("validate_index")
-          options_values <- self[["_options_values"]]
-          len <- length(options_values)
+          log_out("MultipleSelectionWidgetClass$validate_index")
+          log_out(index,use.print=TRUE)
+          options_labels <- self[["_options_labels"]]
+          len <- length(options_labels)
           if(length(index) > 0){
               index <- unique(index)
               index[index >= len] <- len - 1L
               index[index < 0] <- 0L
           } 
-          index
-      },
-      #' @description Standard observer function for indices
-      #' @param name Name of the traitlet
-      #' @param self The traitlet container
-      #' @param index The index
-      observe_index = function(name,self,index){
-          # cat("observe_index")
-          options_values <- self[["_options_values"]]
-          if(length(index) > 0){
-              value <- options_values[index + 1L]
-          } else {
-              value <- NULL
-          }
-          self$traits$value$set(value,notify=FALSE)
-          # cat(value)
-          self$send_state()
-      },
-      #' @description Validate a value
-      #' @param value The value to be validated
-      validate_value = function(value){
-          # cat("validate_value")
-          options_labels <- self[["_options_labels"]]
-          options_values <- self[["_options_values"]]
-          value <- unique(value)
-          if(length(value) == 0){
-              value <- NULL
-          }
-          else if(is.character(value) && all(value %in% options_labels)){
-              i <- match(value,options_labels)
-              value <- options_values[i]
-          }
-          else if(!all(value %in% options_values)){
-              value <- self$value
-          }
-          value
-      },
-      #' @description Standard observer function for values
-      #' @param name Name of the traitlet
-      #' @param self The traitlet container
-      #' @param value A value
-      observe_value = function(name,self,value){
-          # cat("observe_value\n")
-          # print(name)
-          # cat("value\n")
-          # print(value)
-          if(length(value)){
-              options_labels <- self[["_options_labels"]]
-              options_values <- self[["_options_values"]]
-              if(all(value %in% options_labels)){
-                  index <- match(value,options_labels) - 1L
-              } 
-              else if(all(value %in% options_values)){
-                  index <- match(value,options_values) - 1L
-              }
-          }
-          else 
-              index <- integer(0)
-          self$traits$index$set(index,notify=FALSE)
-          # str(self)
-          self$send_state()
+          sort(index)
       },
       #' @description Initialiser
       #' @param options A named vector or a vector coerceable into a character vector.
-      #' @param value A trait.
+      #' @param value Names of selectable options.
       #' @param ... Any other arguments, ignored.
       initialize = function(options,value,...){
-          # print(options)
-          # args <- list(...)
-          # print(args)
+          log_out("MultipleSelectionWidgetClass$validate_index")
           super$initialize(...)
-          if(length(names(options))) {
-              options <- names(options)
-              self[["_options_labels"]] <- options
-          }
-          else {
-              options <- as.character(options)
-              self[["_options_labels"]] <- options
-          }
-          self[["_options_values"]] <- options
+          options <- as.character(options)
+          self[["_options_labels"]] <- options
           self$validate("index",self$validate_index)
-          self$validate("value",self$validate_value)
-          self$observe("value",self$observe_value)
-          self$observe("index",self$observe_index)
           if(!missing(value)){
-              self$value <- value
+              index <- match(value,options,nomatch=0L)
+              index <- index[index > 0L]
+              self$index <- index - 1L
           }
-          else
-              self$value <- options[[1]]
+      }
+  ),
+  active = list(
+      #' @field value The selected option
+      value = function(value){
+          if(missing(value)){
+              index <- self$traits$index$value + 1L
+              options_labels <- self[["_options_labels"]]
+              options_labels[index]
+          } else {
+              options_labels <- self[["_options_labels"]]
+              index <- match(value,options_labels,nomatch=0L)
+              index <- index[index > 0L]
+              if(length(index)){
+                  self$traits$index$set(index - 1L)
+                  self$send_state()
+              }
+          }
       }
   )
 )
@@ -440,7 +326,24 @@ SelectionRangeSliderClass <- R6Class_("SelectionRangeSlider",
       #' @field style A SliderStyle widget
       style = structure(R6Instance(SliderStyleClass),sync=TRUE),
       #' @field behavior A string that describes the ddragging behavior.
-      behavior = structure(StrEnum(c("drag-tap","drag-snap","tap","drag","snap")),sync=has_iw_ver(8))
+      behavior = structure(StrEnum(c("drag-tap","drag-snap","tap","drag","snap")),sync=has_iw_ver(8)),
+      #' @description Validate an index.
+      #' @param index An index, the index to be checked.
+      validate_index = function(index){
+          index <- as.integer(range(index))
+          index
+      },
+      #' @description Initialiser
+      #' @param options A named vector or a vector coerceable into a character vector.
+      #' @param value Names of selectable options.
+      #' @param ... Any other arguments, ignored.
+      initialize = function(options,value,...){
+          super$initialize(options,value,...)
+          index <- self$traits$index$get()
+          index <- self$validate_index(index)
+          self$traits$index$set(index)
+          # self$validate("index",self$validate_index)
+      }
   )
 )
 
