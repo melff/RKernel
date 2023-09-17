2c6c83a40d858a71dac8b009df4b0601dcaca3a4
Martin Elff <elff@newdebian.local>
2023-09-17 22:41:59 +0200

    Added initial support for FileUpload widgets. These do not work yet.
    For some reasons, FileUpload widgets do not work. They show up in the frontend.
    But clicking the button and specifying a file does not lead to a message being
    sent to the kernel. This seems to be an issue with the frontend. However, the
    documentation does not help me (yet) in getting an idea why this does not work
    and what requirements are not met.

---
+++ pkg/R/widget-upload.R
@@ -0,0 +1,48 @@
+#' File Upload Widgets
+#' @description Class and constructor for file upload widgets
+#' @include widget-value.R
+#' @name FileUpload
+NULL
+
+
+#' @rdname FileUpload
+#' @export
+FileUploadClass <- R6Class_("FileUpload",
+   inherit = ValueWidgetClass,
+   public = list(
+       #' @field _model_name Name of the Javascript model in the frontend.
+       `_model_name` = structure(Unicode("FileUploadModel"),sync=TRUE),
+       #' @field _view_name Name of the Javascript view in the frontend.
+       `_view_name` = structure(Unicode("FileUploadView"),sync=TRUE),
+       #' @field accept A character string that defineds the accepted file type.
+       #'    If empty, all files are accepted.
+       accept = structure(Unicode(""),sync=TRUE),
+       #' @field multiple A Boolean traitlet, whether multiple files are accepted.
+       multiple = structure(Boolean(FALSE),sync=TRUE),
+       #' @field disabled A \link{Boolean} traitlet, whether the widget is disabled.
+       disabled = structure(Boolean(FALSE),sync=TRUE),
+       #' @field icon A character string, the font-awesome without the 'fa-' prefix.
+       icon = structure(Unicode("upload"),sync=TRUE),
+       #' @field box_style The string that describes the button style
+       box_style = structure(StrEnum(
+           c("primary","success","info","warning","danger",""),
+           default="")),
+       #' @field style The button style, an object of class "ButtonStyleClass".
+       style = structure(R6Instance(ButtonStyleClass),sync=TRUE),
+       #' @field error A string with an error message, if applicable.
+       error = structure(Unicode(""),sync=TRUE),
+       #' @field value The uploaded data.
+       value = structure(Unicode(length=NA),sync=TRUE,auto_unbox=FALSE),
+       #' @description A generic initializer function
+       #' @param value A value to initialize instance with
+       #' @param ... Any other arguments, ignored.
+       initialize = function(value,...){
+           super$initialize(...)
+           warning("Unfortunately file upload does not work. This seems so because some undocumented requirements are not met at the Javascript frontend side. [Help wanted]")
+       }       
+   ))
+
+
+#' @describeIn FileUpload The FileUpload constructor function
+#' @export
+FileUpload <- function(...) FileUploadClass$new(...)
