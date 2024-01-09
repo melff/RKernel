385a419b12f855b0ca5b450fe5dabe103273753d
Martin Elff <elff@newdebian.local>
2024-01-09 18:23:03 +0100

    Most of variable inspection works - arrays not supported yet.

--- pkg/R/dap-server.R
+++ pkg/R/dap-server.R
@@ -49,50 +49,89 @@ DAPServer <- R6Class("DAPServer",
           return(NULL)
       },
       inspect_variables = function(request){
+          self$children <- list()
           body <- NULL
           e <- self$envir
           varnames <- ls(e)
           if(length(varnames)){
-              variables <- lapply(varnames,self$inspect_variable,envir=e)
+              variables <- lapply(varnames,self$inspect_thing)
               body <- list(variables = unname(variables))
           }
           return(body)
       },
-      inspect_variable = function(varname,envir){
-          var <- get(varname,envir=envir)
-          log_out(var,use.str=TRUE)
+      inspect_thing = function(path,name=tail(path,n=1L)){
+          desc <- self$get_thing(path)
+          x <- desc$value
+          ename <- desc$ename
           vref <- 0L
-          if(is.atomic(var)) {
-              if(length(var) == 1){
-                  value <- format(var)
-                  type <- typeof(var)
-                  type <- paste0(type," ",value)
+          if(is.atomic(x)) {
+              if(length(x) == 1){
+                  value <- format(x)
+                  type <- class(x)
+                  if(length(path) == 1)
+                      type <- paste0(type," ",value)
+                  else if(type %in% names(self$types)) 
+                      type <- self$types[type]
               }
               else {
-                  type <- self$get_atomic_type(var)
-                  value <- self$get_atomic_val(var)
-                  if(is.vector(var)){
-                      vref <- self$vref_counter
-                      self$vref_counter <- self$vref_counter + 1L
-                      self$var_children[[vref]] <- list(name=varname,envir=envir,start_index=1L)
+                  type <- self$get_atomic_type(x)
+                  value <- self$get_atomic_val(x)
+                  if(self$is.vectorlike(x)){
+                      vref <- self$childref(path,start=1L)
                   }
               }
           }
+          else if(is.function(x)){
+              type <- "function"
+              value <- "<function>"
+              vref <- self$childref(path,start=1L)
+          }
           else {
-              type <- self$get_structured_type(var)
-              value <- self$get_structured_val(var)
-              vref <- self$vref_counter
-              self$vref_counter <- self$vref_counter + 1L
-              self$var_children[[vref]] <- list(name=varname,envir=envir,start_index=1L)
+              type <- self$get_structured_type(x)
+              value <- self$get_structured_val(x)
+              vref <- self$childref(path,start=1L)
           }
           res <- list(
-              name = varname,
+              name = name,
               value = value,
               type = type,
-              evaluateName = varname,
+              evaluateName = ename,
               variablesReference = vref
           )
           return(res)
+      },
+      childref = function(path,start){
+          vref <- self$path2vref(path=path,start=start)
+          if(vref == 0){
+              vref <- self$vref_counter
+              self$children[[vref]] <- list(path=path,start=start)
+              self$vref_counter <- vref + 1L
+          }
+          return(vref)
+      },
+      get_thing = function(path){
+          ename <- path[[1]]
+          x <- get0(ename,envir=self$envir)
+          for(i in tail(path,-1)){
+              if(i == "@"){
+                  x <- attributes(x)
+                  ename <- sprintf("attributes(%s)",ename)
+              }
+              else if(is.list(x)){
+                  if(is.numeric(i) && i <= length(x) || is.character(i))
+                      x <- x[[i]]
+                  else return(NULL)
+                  if(is.character(i))
+                      ename <- paste0(ename,"$",i)
+                  else
+                      ename <- paste0(ename,"[[",i,"]]")
+              }
+              else {
+                  x <- x[i]
+                  ename <- paste0(ename,"[",i,"]")
+              }
+          }
+          return(list(value=x,ename=ename,path=path))
       },
       get_atomic_type = function(x){
           gsub("NULL ...","",capture.output(str(x,vec.len=0L,give.attr=FALSE)))
@@ -111,31 +150,67 @@ DAPServer <- R6Class("DAPServer",
           #log_info(request,use.print=TRUE)
           return(NULL)
       },
-      reply_variables = function(request){
-          log_out(">> reply_variables")
-          vref <- request$arguments$variablesReference
-          children_desc <- self$var_children[[vref]]
-          if(!length(children_desc))
-              return(NULL)
-          e <- children_desc$envir
-          varname <- children_desc$name
-          var <- get0(varname,e)
-          if(!length(var)) return(NULL)
-          l <- length(var)
-          i0 <- children_desc$start_index
-          if(is.list(var))
+      inspect_children = function(vref){
+          desc <- self$children[[vref]]
+          if(!length(desc)) return(NULL)
+          path <- desc$path
+          start <- desc$start
+          thing <- self$get_thing(path)
+          if(!length(thing)) return(NULL)
+          ename <- thing$ename
+          x <- thing$value
+          l <- length(x)
+          i0 <- desc$start
+          if(is.list(x))
               m <- self$list_chunk_size
           else 
               m <- self$vector_chunk_size
           n <- min(m, l - i0 + 1L)
           ii <- seq.int(from=i0,
                         length=n)
-          if(is.atomic(var) && is.vector(var)) self$show_vector(var, ii, varname, l)
-          else if(is.list(var)) self$show_list(var,ii,varname,l)
-          else return(NULL)
+          if(self$is.vectorlike(x)) self$show_vector(thing, ii,l)
+          else if(is.list(x)) self$show_list(thing,ii,path,l)
+          else if(is.function(x)) self$show_function(thing)
+          else NULL
       },
-      show_vector = function(x,ii,name,l){
-          log_out(">>>>> show_vector")
+      reply_variables = function(request){
+          vref <- request$arguments$variablesReference
+          list(variables=self$inspect_children(vref))
+      },
+      add_atts = function(thing){
+          path <- thing$path
+          ename <- thing$ename
+          if(tail(path,1L)=="@") return(NULL) # Avoid infinite recursion
+          att_path <- c(path,list("@"))
+          vref <- self$childref(att_path,start=1L)
+          list(list(
+              name = "  - attributes",
+              type = "str",
+              value = "...",
+              evaluateName = sprintf("attributes(%s)",ename),
+              variablesReference = vref
+          ))
+      },
+      more_elts = function(thing,n,l){
+          path <- thing$path
+          ename <- thing$ename
+          vref <- self$childref(path,start=n)
+          list(list(
+                  name = paste0("[",n,":",l,"]"),
+                  type = "str",
+                  value = "...",
+                  evaluateName = paste0(ename,"[",n,":",l,"]"),
+                  variablesReference = vref
+          ))
+      },
+      is.vectorlike = function(x){
+          # A more 'inclusive' variant of 'is.vector'
+          is.atomic(x) && !is.array(x) || (isS4(x) && is.vector(x@.Data))
+      },
+      show_vector = function(thing,ii,l){
+          x <- thing$value
+          ename <- thing$ename
+          path <- thing$path
           nms <- names(x)
           if(!length(nms))
               nms <- paste0("[",ii,"]")
@@ -144,26 +219,27 @@ DAPServer <- R6Class("DAPServer",
               nms[zch] <- paste0("[",ii[zch],"]")
           nms <- format(nms)
           vals <- format(x[ii])
-          type <- typeof(x) 
+          type <- class(x) 
           if(type %in% names(self$types)) 
               type <- self$types[type]
-          enms <- paste0(name,"[",ii,"]")
-          variables <- Map(self$show_elt,vals,nms,enms,type)
+          else
+              type <- "str" # To avoid NaN in the frontend
+          enms <- paste0(ename,"[",ii,"]")
+          elements <- Map(self$show_elt,vals,nms,enms,type)
           if(max(ii) < l){
               n <- max(ii) + 1L
-              more_elts <- list(
-                  name = paste0("[",n,":",l,"]"),
-                  type = "str",
-                  value = "...",
-                  evaluateName = paste0(name,"[",n,":",l,"]"),
-                  variablesReference=0L
-              )
-              variables <- c(variables,list(more_elts))
+              more_elts <- self$more_elts(thing,n,l)
+              elements <- c(elements,more_elts)
           }
-          list(variables=unname(variables))
+          if(length(attributes(x))){
+              att_el <- self$add_atts(thing)
+              elements <- c(att_el,elements)
+          }
+          unname(elements)
       },
-      show_list = function(x,ii,name,l){
-          log_out(">>>>> show_list")
+      show_list = function(thing,ii,path,l){
+          x <- thing$value
+          ename <- thing$ename
           nms <- names(x)
           if(!length(nms))
               nms <- paste0("[[",ii,"]]")
@@ -172,50 +248,49 @@ DAPServer <- R6Class("DAPServer",
               nms[zch] <- paste0("[[",ii[zch],"]]")
           nms <- format(nms)
           vals <- x[ii]
-          enms <- paste0(name,"[[",ii,"]]")
-          variables <- Map(self$show_list_elt,vals,nms,enms)
+          enms <- paste0(ename,"[[",ii,"]]")
+          elements <- list()
+          for(i in ii){
+              elements[[i]] <- self$inspect_thing(c(path,list(i)),name=nms[i])
+          }
           if(max(ii) < l){
               n <- max(ii) + 1L
-              more_elts <- list(
-                  name = paste0("[",n,":",l,"]"),
-                  type = "str",
-                  value = "...",
-                  evaluateName = paste0(name,"[",n,":",l,"]"),
-                  variablesReference=0L
-              )
-              variables <- c(variables,list(more_elts))
+              more_elts <- self$more_elts(thing,n,l)
+              elements <- c(elements,more_elts)
           }
-          list(variables=unname(variables))
+          if(length(attributes(x))){
+              att_el <- self$add_atts(thing)
+              elements <- c(att_el,elements)
+          }
+          unname(elements)
       },
-      show_elt = function(val,name,ename,type){
+      show_function = function(thing,...){
+          f <- thing$value
+          args <- format(formals(f))
+          vals <- unname(args)
+          vals[!nzchar(vals)] <- "<no default>"
+          nms <- names(args)
+          elements <- Map(self$show_elt,vals,nms,"","str")
+          unname(elements)
+      },
+      show_elt = function(x,name,ename,type){
           list(name=name,
-               value=val,
+               value=x,
                type=type,
                evaluateName=ename,
                variablesReference=0L)
       },
-      show_list_elt = function(val,name,ename){
-          log_out(">>>>>>>>>> show_list_elt")
-          if(is.atomic(val)) {
-              if(length(val) == 1){
-                  value <- format(val)
-                  type <- typeof(val)
-                  type <- paste0(type," ",value)
-              }
-              else {
-                  type <- self$get_atomic_type(val)
-                  value <- self$get_atomic_val(val)
-              }
+      paths_equal = function(path1,path2){
+          path1 <- paste0(unlist(path1),collapse="_")
+          path2 <- paste0(unlist(path2),collapse="_")
+          path1 == path2
+      },
+      path2vref = function(path,start=1L){
+          for(i in seq_along(self$children)){
+              child <- self$children[[i]]
+              if(self$paths_equal(path,child$path) && start==child$start) return(i)
           }
-          type <- self$get_structured_type(val)
-          value <- self$get_structured_val(val)
-          list(
-              name = name,
-              value = value,
-              type = type,
-              evaluateName = ename,
-              variablesReference = 0L
-          )
+          return(0)
       },
       is_started = FALSE,
       kernel = NULL,
@@ -223,12 +298,14 @@ DAPServer <- R6Class("DAPServer",
       types = c(
           "integer"="int",
           "logical"="bool",
-          "double"="float",
-          "character"="str"
+          "numeric"="float",
+          "character"="str",
+          "factor"="str"
       ),
       vref_counter = 1L,
-      var_children = list(),
-      vector_chunk_size = 25L,
+      children = list(),
+      vector_chunk_size = 10L,
       list_chunk_size = 100L
   )
 )
+
