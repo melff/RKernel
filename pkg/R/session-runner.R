972d6ac3def89fdc7882e644dde59f97f9c8b266
Martin Elff <martin@elff.eu>
2025-01-10 21:27:07 +0100

    First draft of RSessionRunner class

---
+++ pkg/R/session-runner.R
@@ -0,0 +1,223 @@
+RSessionRunner <- R6Class("RSessionRunner",
+  public = list(
+    kernel = NULL,
+    session = NULL,
+    repl = NULL,
+    initialize = function(kernel,
+                          stream = kernel$stream) {
+      self$kernel <- kernel
+      private$kernel_stream <- stream
+    },
+    start = function() {
+      self$session <- RKernelSession$new()
+      self$session$connect(kernel = kernel,yield = NULL)
+      self$repl <- RSessionAdapter$new(
+        session = self$session,
+        stdout_callback =  private$handle_stdout,
+        stderr_callback =  private$handle_stderr,
+        browser_callback = private$handle_browser,
+        input_callback =   private$readline)
+      kernel <- self$kernel
+      private$shell_parent <- kernel$save_shell_parent()
+      self$session$start()
+      private$start_graphics()
+      private$stdout_filter <- MessageFilter$new(
+          text_handler = self$stdout,
+          msg_handler  = private$handle_msg
+        )
+      private$stderr_filter <- MessageFilter$new(
+          text_handler = self$stderr,
+          msg_handler  = private$handle_msg
+        )
+    },
+    stop = function() {
+      self$repl <- NULL
+      self$session$close()
+    },
+    restart = function() {
+      self$stop()
+      self$start()
+    },
+    run = function(code, ..., new_graphics_display = FALSE) {
+      self$repl$run_code(code,...)
+    },
+    set_shell_parent = function(msg) {
+      private$shell_parent = msg
+    },
+    stdout = function(txt) {
+      self$stream(txt, stream = "stdout")
+    },
+    stderr = function(txt) {
+      self$stream(txt, stream = "stderr")
+    },
+    stream = function(txt, stream) {
+      kernel <- self$kernel
+      kernel$restore_shell_parent(private$shell_parent)
+      private$kernel_stream(txt,stream)
+      private$display_changed_graphics()
+    },
+    send_display = function(d) {
+      kernel <- self$kernel
+      kernel$restore_shell_parent(private$shell_parent)
+      kernel$send_display(d)
+    },
+    install_msg_handlers = function(...) {
+      l <- list(...)
+      nms <- names(l)
+      if(length(l) == 1 && !length(nms)) {
+        l <- l[[1]]
+        nms <- names(l)
+      }
+      for(n in nms) {
+        private$msg_handlers[[n]] <- l[[n]]
+      }
+    },
+    settings_set = function(name,value) {
+      assign(name, value, envir = private$settings)
+    },
+    settings_get = function(name, default = NULL) {
+      get0(name, envir = private$settings, ifnotfound = default)
+    },
+    set_callback = function(name, FUN) {
+      assign(name, FUN, envir = private$callbacks)
+    }
+  ),
+  private = list(
+
+    kernel_stream = NULL,
+    handle_stdout = function(text) {
+      private$stdout_filter$process(text)
+    },
+    handle_stderr = function(text) {
+      private$stderr_filter$process(text)
+    },
+    msg_handlers = new.env(),
+    settings = new.env(),
+
+    handle_msg = function(msg) {
+      if(!is.list(msg)) {
+        err_msg <- "R session sent an invalid message - not a list"
+        self$stderr(err_msg)
+        log_error(err_msg, traceback = FALSE)
+        dep_msg <- deparse0(msg)
+        self$stderr("\n")
+        self$stderr(dep_msg)
+        self$stderr("\n")
+        log_error(dep_msg, traceback = FALSE)
+        return(NULL)
+      }
+      msg_type <- msg$type
+      # log_out(msg_type)
+      msg_handler <- private$msg_handlers[[msg_type]]
+      if(is.function(msg_handler)){
+        msg_handler(msg)
+      } else {
+        w_msg <- sprintf("R session sent message of unknown type '%s'", msg_type)
+        # log_out(msg_handler, use.str = TRUE)
+        self$stderr(w_msg)
+        dep_msg <- deparse0(msg)
+        self$stderr("\n")
+        self$stderr(dep_msg)
+        self$stderr("\n")
+        log_warning(paste(w_msg,dep_msg,sep=":\n "))
+      }
+    },
+    handle_browser = function(prompt) {
+      dbgConsole(session = self$session,
+                 prompt = prompt,
+                 use_widgets = self$settings_get("use_widgets"))
+      if(self$settings_get("browser_in_condition"))
+        self$repl$process_output(until_prompt=TRUE)
+      self$kernel$restore_shell_parent(self$shell_parent)
+      return(TRUE)
+    },
+
+    handle_event_msg = function(msg) {
+      # log_out("handle_event_msg")
+      # log_str(msg)
+      event <- msg$content$event
+      plot_id <- msg$content$plot_id
+      switch(event,
+             recover = ,
+             debugger = set_config(browser_in_condition = TRUE),
+             "recover-finished" = ,
+             "debugger-finished" = set_config(browser_in_condition = FALSE),
+             new_plot = private$handle_new_plot(plot_id = plot_id)
+             )
+    },
+
+    handle_menu_request = function(msg) {
+      
+      kernel <- self$kernel
+      saved_parent <- kernel$save_shell_parent()
+      Menu(kernel = kernel,
+           args = msg$content)
+      kernel$restore_shell_parent(saved_parent) 
+      return(TRUE)
+    },
+
+    readline = function(prompt="") {
+      self$kernel$readline(prompt=prompt)
+    },
+    start_graphics = function(...) {
+      gdetails <- self$session$start_graphics()
+      private$graphics = GraphicsObserver$new(gdetails)
+    },
+    stdout_filter = NULL,
+    stderr_filter = NULL,
+    shell_parent = NULL,
+
+    graphics = NULL,
+    handle_new_plot = function(plot_id) {
+      # log_out("handle_new_plot")
+      if(length(private$graphics_plot_id) &&
+         plot_id == private$graphics_plot_id) {
+        private$display_changed_graphics()
+      }
+      else {
+        private$graphics_display(plot_id)
+      }
+    },
+
+    graphics_display_id = character(0),
+    graphics_plot_id = NULL,
+    graphics_display = function(plot_id, 
+                                update = FALSE) {
+      # log_out("graphics_display")
+      if(update) {
+        display_id <- private$graphics_display_id
+      } else {
+        display_id <- UUIDgenerate()
+      }
+      d <- private$graphics$display_data(plot_id,
+                                          display_id = display_id,
+                                          update = update)
+      self$display_send(d)
+      private$graphics_plot_id <- plot_id
+      private$graphics_display_id <- display_id
+    },
+
+    display_changed_graphics = function() {
+      # log_out("display_changed_graphics")
+      if(self$session$sleeping()) {
+        plot_id <- as.character(private$graphics$active_id())
+        poll_res <- private$graphics$poll()
+        if(poll_res["active"]) {
+          if(poll_res[2]) { # New plot
+            # log_out("New plot")
+            private$graphics_display(plot_id)
+          }
+          else if(poll_res[3]) { # Plot update
+            # log_out("Plot update")
+            if(plot_id != private$graphics_plot_id) {
+              log_warn("We seem to have lost a plot ...")
+            }
+            update <- self$get_setting("reuse_graphics_display",TRUE)
+            private$graphics_display(plot_id,
+                                    update = update)
+          }
+        }
+      }
+    }
+  )
+)
