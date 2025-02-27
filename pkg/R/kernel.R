de236287f62bf3cdeddeac4753fcffb7915dde4e
Martin Elff <martin@elff.eu>
2025-02-27 20:49:46 +0100

    Use startup files for RKernel and RKernelSession

--- pkg/R/kernel.R
+++ pkg/R/kernel.R
@@ -81,6 +81,7 @@ Kernel <- R6Class("Kernel",
                       height="5in")
         self$display_send(frm)
       })
+      private$run_startup()
     },
     #' @field session See \code{\link{RKernelSession}}.
     session = list(),
@@ -1367,6 +1368,28 @@ Kernel <- R6Class("Kernel",
         msg <- sprintf("Stopped detached session with name '%s'\n", name)
         self$stdout(msg)
       }
+    },
+
+    run_startup = function(...) {
+      log_out("run_startup()")
+      home_dir <- Sys.getenv("HOME")
+      jupyter_config <- file.path(home_dir, ".jupyter", "RKernel-config.R")
+      if (file.exists(jupyter_config)) {
+        source(jupyter_config)
+      }
+      parents <- strsplit(getwd(), .Platform$file.sep)[[1]]
+      parents <- Reduce(file.path, parents, accumulate = TRUE)
+      for (parent in parents) {
+        dot_file <- file.path(parent, ".RKernel-profile")
+        if (file.exists(dot_file)) {
+          source(dot_file)
+        }
+        startup_file <- file.path(parent, "RKernel-startup.R")
+        if (file.exists(startup_file)) {
+          source(startup_file)
+        }
+      }
+      log_print(options())
     }
   )
 )
--- pkg/R/startup.R
+++ pkg/R/startup.R
@@ -2,18 +2,18 @@ config <- new.env()
 
 startup <- function(){
   home_dir <- Sys.getenv("HOME")
-  jupyter_config <- file.path(home_dir, ".jupyter", "RKernel-config.R")
+  jupyter_config <- file.path(home_dir, ".jupyter", "RKernelSession-config.R")
   if (file.exists(jupyter_config)) {
     source(jupyter_config)
   }
   parents <- strsplit(getwd(), .Platform$file.sep)[[1]]
   parents <- Reduce(file.path, parents, accumulate = TRUE)
   for (parent in parents) {
-    dot_file <- file.path(parent, ".RKernel-profile")
+    dot_file <- file.path(parent, ".RKernelSession-profile")
     if (file.exists(dot_file)) {
       source(dot_file)
     }
-    startup_file <- file.path(parent, "RKernel-startup.R")
+    startup_file <- file.path(parent, "RKernelSession-startup.R")
     if (file.exists(startup_file)) {
       source(startup_file)
     }
