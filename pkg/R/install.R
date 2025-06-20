6b0a157b969afc7bbe99ae47cd456a5e27f6c7bd
Martin Elff <martin@elff.eu>
2025-06-20 12:19:37 +0200

    Update documentation

--- pkg/R/install.R
+++ pkg/R/install.R
@@ -11,12 +11,23 @@ install <- function(){
 #' @importFrom jsonlite fromJSON toJSON read_json write_json
 #' @param user Logical, whether to install the kernel in the user's home directory
 #' @param prefix NULL or a character string with a path prefix
+#' @param kernel_name String; the name of the kernel. To be used 
+#'      e.g. as the '--kernel=' argument for the CLI call 'jupyter console'
+#' @param display_name String; the name as it appears in the Jupyter
+#'      web interface
+#' @param env A list or NULL; optional environmental variables
+#'      for the R kernel process. For example, to get single-threaded 
+#'      open-blas use 
+#'      "\code{env = list(OPENBLAS_NUM_THREADS = 1)}".
+#'      To force a single OMP thread use
+#'      "\code{env = list(OMP_NUM_THREADS = 1)}"
 #' @export
 # The following is adapted from IRKernel/R/installspec.r
 installspec <- function(user = TRUE,
                         prefix = NULL,
                         kernel_name = "rkernel",
-                        display_name = "R"){
+                        display_name = "R",
+                        env = NULL){
    kernelspec_srcdir <- system.file("kernelspec",package="RKernel")
    tmp_dir <- tempfile()
    dir.create(tmp_dir)
