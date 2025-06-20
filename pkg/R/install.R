851cd39501dd75bb53c9e5f9eca966d5846a4673
Martin Elff <martin@elff.eu>
2025-06-20 12:09:02 +0200

    Make kernel install more flexible

--- pkg/R/install.R
+++ pkg/R/install.R
@@ -11,56 +11,51 @@ install <- function(){
 #' @importFrom jsonlite fromJSON toJSON read_json write_json
 #' @param user Logical, whether to install the kernel in the user's home directory
 #' @param prefix NULL or a character string with a path prefix
-#' @param single_blas Logical, whether the kernel should use a single-threaded BLAS library.
 #' @export
 # The following is adapted from IRKernel/R/installspec.r
-installspec <- function(user=TRUE,prefix=NULL,single_blas=FALSE){
+installspec <- function(user = TRUE,
+                        prefix = NULL,
+                        kernel_name = "rkernel",
+                        display_name = "R"){
    kernelspec_srcdir <- system.file("kernelspec",package="RKernel")
    tmp_dir <- tempfile()
    dir.create(tmp_dir)
    file.copy(kernelspec_srcdir,tmp_dir,recursive=TRUE)
    
-   json_infile <- file.path(tmp_dir,"kernelspec","RKernel.json")
    json_outfile <- file.path(tmp_dir,"kernelspec","kernel.json")
-   kernelspec <- read_json(json_infile)
    # Put the absolute path of the current interpreter
    kernelspec$argv[[1]] <- file.path(R.home("bin"),"R")
+   kernelspec$display_name = display_name
    write_json(kernelspec,pretty=TRUE,auto_unbox=TRUE,
          path=json_outfile)
-   unlink(json_infile)
    jupyter_call <- c(
        "jupyter",
        "kernelspec",
        "install",
        "--replace",
-       "--name","rkernel",
+       "--name", name,
        if(user) "--user" else NULL,
        if(length(prefix)) paste0("--prefix=",prefix),
        file.path(tmp_dir,"kernelspec")
    )
    exit_code <- system2(jupyter_call)
 
-   if(single_blas){ 
-       json_infile <- file.path(tmp_dir,"kernelspec","RKernel1blas.json")
-       json_outfile <- file.path(tmp_dir,"kernelspec","kernel.json")
-       kernelspec <- fromJSON(json_infile)
-       # Put the absolute path of the current interpreter
-       kernelspec$argv[[1]] <- file.path(R.home("bin"),"R")
-       write(toJSON(kernelspec,pretty=TRUE,auto_unbox=TRUE),
-             file=json_outfile)
-       unlink(json_infile)
-       jupyter_call <- c(
-           "jupyter",
-           "kernelspec",
-           "install",
-           "--replace",
-           "--name","rkernel1blas",
-           if(user) "--user" else NULL,
-           if(length(prefix)) paste0("--prefix=",prefix),
-           file.path(tmp_dir,"kernelspec")
-       )
-       exit_code <- system2(jupyter_call)
-   }
    unlink(tmp_dir,recursive=TRUE)
    invisible(exit_code)
 }
+
+kernelspec <- list(
+    argv = list(
+        "R",
+        "--slave",
+        "--vanilla", 
+        "-e", 
+        "RKernel::main()", 
+        "--args", 
+        "{connection_file}"
+    ),
+    language = "R",
+    metadata = list(
+        debugger = FALSE
+    )
+)
