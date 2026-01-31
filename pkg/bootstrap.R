e09fa523176746f0b09a6686c2ae9f0948fcd430
Martin Elff <martin@elff.eu>
2026-02-01 00:45:31 +0100

    Remove superfluous 'bootstrap.R' file

--- pkg/bootstrap.R
+++
@@ -1,8 +0,0 @@
-cran <- findCRANmirror("web")
-avpaks <- available.packages(contrib.url(cran))
-if(!("httpgd" %in% rownames(avpaks))) {
-  install.packages(
-    'httpgd', 
-    repos = c('https://nx10.r-universe.dev', 
-              cran))
-}
